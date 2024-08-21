0b451c7436564c7287ac64d3fad15307f08b8216
roman <roman.pavlenko.job@gmail.com>
2024-08-21 17:19:44 +0300

    created grid for calendar basis

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,9 @@ export default function Home() {
   const [allDaysCount, setAllDaysCount] = useState<number[]>();
 
   const [currentDate, setCurrentDate] = useState<number>();
+
   const [month, setMonth] = useState<number>();
+  const [year, setYear] = useState<number>()
 
   const [numberOfDays, setNumberOfDays] = useState<number>();
 
@@ -32,6 +34,7 @@ export default function Home() {
     const date = Date.now()
     setCurrentDate(date);
     setMonth(new Date(date).getMonth());
+    setYear(new Date(date).getFullYear());
     setNumberOfDays(DaysInMonth(new Date(date).getMonth(), new Date(date).getFullYear()));
     setCurrentDayOfWeek(DayOfWeek(new Date(date).getMonth(), new Date(date).getFullYear(), new Date(date).getDate()))
     setAllDaysCount(Array.from({length: DaysInMonth(new Date(date).getMonth(), new Date(date).getFullYear())}, (_, i) => i + 1))
@@ -41,20 +44,62 @@ export default function Home() {
     <main className={styles.main}>
       <div>
         {
-          <div>
+          (month) ? allMonthsNames[month] : ""
+        }
+        {
+          (currentDayOfWeek) ? allDaysNames[currentDayOfWeek] : ""
+        }
+        {
+          <div className={styles.calendar_table}>
             {
-              (month) ? allMonthsNames[month] : ""
-            }
-            {
-              (currentDayOfWeek) ? allDaysNames[currentDayOfWeek] : ""
-            }
-            {
-              (allDaysCount) ?
+              (allDaysCount && month && year) ?
               allDaysCount.map((el, index) => {
-                return(
-                  <div>123</div>
-                )
-              }) : ""
+                switch (allDaysNames[DayOfWeek(month, year, el)]) {
+                  case "Monday": 
+                  return(
+                    <div className={`${styles.monday}`}>
+                      {el}
+                    </div>
+                  );
+                  case "Tuesday": 
+                  return(
+                    <div className={`${styles.tuesday}`}>
+                      {el}
+                    </div>
+                  );
+                  case "Wednesday": 
+                  return(
+                    <div className={`${styles.wednesday}`}>
+                      {el}
+                    </div>
+                  );
+                  case "Thursday": 
+                  return(
+                    <div className={`${styles.thursday}`}>
+                      {el}
+                    </div>
+                  );
+                  case "Friday": 
+                  return(
+                    <div className={`${styles.friday}`}>
+                      {el}
+                    </div>
+                  );
+                  case "Saturday": 
+                  return(
+                    <div className={`${styles.saturday}`}>
+                      {el}
+                    </div>
+                  );
+                  case "Sunday": 
+                  return(
+                    <div className={`${styles.sunday}`}>
+                      {el}
+                    </div>
+                  );
+                }
+                }
+              ) : ""
             } 
           </div>
         }
